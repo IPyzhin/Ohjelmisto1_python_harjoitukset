462b836bce6126e6e2a217559656a2e479af7129
IPyzhin <ivan.pyzhin@gmail.com>
2026-01-14 16:28:55 +0200

    Updated

---
+++ Module2/Assignment 5.py
@@ -0,0 +1,7 @@
+talents = float(input("Enter talents: "))
+pounds = float(input("Enter pounds: "))
+lots = float(input("Enter lots: "))
+total_grams = (13.3 * lots) + (13.3 * 32 * pounds) + (13.3 * 32 * 20 * talents)
+kilograms = int(total_grams // 1000)
+remaining_grams = total_grams % 1000
+print(f"The weight in modern units: \n{kilograms} kilograms and {remaining_grams:.2f} grams.")
---
+++ Module2/Assignment 6.py
@@ -0,0 +1,12 @@
+import random
+digi3_num1 = random.randint(0,9)
+digi3_num2 = random.randint(0,9)
+digi3_num3 = random.randint(0,9)
+digitcode3 = str(digi3_num1) + str(digi3_num2) + str(digi3_num3)
+digi4_num1 = random.randint(1,6)
+digi4_num2 = random.randint(1,6)
+digi4_num3 = random.randint(1,6)
+digi4_num4= random.randint(1,6)
+digitcode4 = str(digi4_num1) + str(digi4_num2) + str(digi4_num3) + str(digi4_num4)
+print("3-digit code:", digitcode3, "\n4-digit code:", digitcode4)
+
